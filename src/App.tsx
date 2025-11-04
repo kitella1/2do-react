5102ee77423c736d1b25ea7e6ca6cefde74196ea
Katie <kitkatkadams@gmail.com>
2025-11-04 12:21:38 +0000

    Initial style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import twoDoLogo from "./assets/logo.png";
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./stores/counter/counterSlice";
-import type { CounterStore } from "./types/counter";
 
 function App() {
-	const count = useSelector((state: CounterStore) => state.counter.value);
-	const dispatch = useDispatch();
-
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1 data-testid="title">Vite + React</h1>
-			<div className="card">
-				<button onClick={() => dispatch(increment())}>count is {count}</button>
-				<button onClick={() => dispatch(decrement())}>Reduce count</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<header>
+				<img
+					src={twoDoLogo}
+					id="logo"
+					alt="the 2Do app logo, pronounced phonetically as To Do"
+				/>
+				<h1 data-testid="title" id="title">
+					Organize your day...
+				</h1>
+			</header>
+
+			<main>
+				<div className="flexBox">{/* Form goes here with inputs */}</div>
+				<div className="flexBox">{/* Filter and sort buttons goes here */}</div>
+				<div className="flexBox">
+					Cards go here
+					{/* TODO: Make cardsContainer with overflow rules */}
+				</div>
+			</main>
+			<footer>Developed by Katie Adams, 2025</footer>
 		</>
 	);
 }
