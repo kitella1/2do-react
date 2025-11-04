c240c2f7ffa1bc121046a5a5987c356a7ff66046
Katie <kitkatkadams@gmail.com>
2025-11-04 16:44:14 +0000

    Unit tests written

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,20 +2,16 @@ import { convertToKebabCase } from "../utils";
 
 interface DropdownProps {
 	label: string;
-	defaultValue: string;
 	// The key will be used as the value
 	options: string[];
 }
 
-export default function Dropdown({
-	label,
-	defaultValue,
-	options,
-}: DropdownProps) {
+export default function Dropdown({ label, options }: DropdownProps) {
 	const selectItems = options.map((option) => (
 		<option
+			key={convertToKebabCase(option)}
+			data-testid={convertToKebabCase(option)}
 			value={convertToKebabCase(option)}
-			selected={defaultValue === option}
 			className="bg-white text-gray-950"
 		>
 			{option}
@@ -28,7 +24,10 @@ export default function Dropdown({
 			className="flex flex-col uppercase text-xs md:text-base font-semibold tracking-wide gap-y-1 relative"
 		>
 			{label}
-			<select className="w-full max-w-80 md:max-w-96 border rounded-xs md:rounded-sm border-white placeholder:text-sm placeholder:text-gray-950/40 placeholder:align-top font-normal p-1 bg-gray-50 text-gray-950">
+			<select
+				data-testid="select"
+				className="w-full max-w-80 md:max-w-96 border rounded-xs md:rounded-sm border-white placeholder:text-sm placeholder:text-gray-950/40 placeholder:align-top font-normal p-1 bg-gray-50 text-gray-950"
+			>
 				{selectItems}
 			</select>
 		</label>
