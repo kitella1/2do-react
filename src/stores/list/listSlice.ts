5d2c2ab3f3beee4ddc07e21c2ac8e8e00a1b4cd9
Katie <kitkatkadams@gmail.com>
2025-11-06 15:03:48 +0000

    First iteration of list store

--- src/stores/list/listSlice.ts
+++ src/stores/list/listSlice.ts
@@ -1,20 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { removeDuplicatesFromArray } from "../../utils";
+
+interface ListItem {
+	id: string;
+	content: string;
+	category: string;
+	isComplete: boolean;
+}
+
+interface Filter {
+	filterType: "category"; // Can be expanded as new filters are needed
+	values: string[];
+}
 
 const counterSlice = createSlice({
-	name: "counter",
-	initialState: { value: 0 },
+	name: "list",
+	initialState: {
+		list: [
+			{ id: "item-1", content: "Walk the dog", isComplete: false },
+		] as ListItem[],
+		filters: [] as Filter[],
+	},
 	reducers: {
-		increment: (state) => {
-			state.value += 1;
+		addListItem: (state, action: { type: string; payload: ListItem }) => {
+			state.list.unshift(action.payload);
 		},
-		decrement: (state) => {
-			state.value -= 1;
+		completeListItem: (state, action: { type: string; payload: string }) => {
+			const itemToComplete = state.list.findIndex(
+				(item) => item.id === action.payload
+			);
+			state.list[itemToComplete].isComplete = true;
 		},
-		incrementByAmount: (state, action) => {
-			state.value += action.payload;
+		applyFilter: (state, action: { type: string; payload: Filter }) => {
+			const filterAlreadyActive = state.filters.findIndex(
+				(item) => item.filterType === action.payload.filterType
+			);
+			// if already filtering by that filter type
+			if (filterAlreadyActive) {
+				const allFiltersForType = state.filters[
+					filterAlreadyActive
+				].values.concat(action.payload.values);
+				state.filters[filterAlreadyActive].values =
+					removeDuplicatesFromArray(allFiltersForType);
+			} else {
+				state.filters.push(action.payload);
+			}
 		},
 	},
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { addListItem, completeListItem, applyFilter } =
+	counterSlice.actions;
 export default counterSlice.reducer;
