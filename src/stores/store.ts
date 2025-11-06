4cef0bd5892ac8946235d110c2c18d08e4b1c005
Katie <kitkatkadams@gmail.com>
2025-11-06 12:57:25 +0000

    Created new listItem direction and store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counter/counterSlice";
+import listReducer from "./list/listSlice";
 
 const store = configureStore({
 	reducer: {
-		counter: counterReducer,
+		counter: listReducer,
 	},
 });
 
