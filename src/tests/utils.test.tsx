f5de9e1a7f6428889a43bca3f7a9b960c8c662ae
Katie <kitkatkadams@gmail.com>
2025-11-06 13:59:23 +0000

    Remove duplicates from array function

--- src/tests/utils.test.tsx
+++ src/tests/utils.test.tsx
@@ -1,31 +1,75 @@
-import { convertToKebabCase } from "../utils";
+import { convertToKebabCase, removeDuplicatesFromArray } from "../utils";
 
 describe("utils.test.tsx", () => {
 	describe("convertToKebabCase", () => {
-		it("single word title", async () => {
+		it("single word title", () => {
 			const bookTitle = "Dune";
 
 			expect(convertToKebabCase(bookTitle)).toBe("dune");
 		});
-		it("two word title", async () => {
+		it("two word title", () => {
 			const bookTitle = "Jurassic Park";
 
 			expect(convertToKebabCase(bookTitle)).toBe("jurassic-park");
 		});
-		it("multiple word title", async () => {
+		it("multiple word title", () => {
 			const bookTitle = "How to Train Your Dragon";
 
 			expect(convertToKebabCase(bookTitle)).toBe("how-to-train-your-dragon");
 		});
-		it("end spaces", async () => {
+		it("end spaces", () => {
 			const bookTitle = "          How to Train Your Dragon  ";
 
 			expect(convertToKebabCase(bookTitle)).toBe("how-to-train-your-dragon");
 		});
-		it("random casing", async () => {
+		it("random casing", () => {
 			const bookTitle = "   hOw To TrAiN yOuR dRaGoN  ";
 
 			expect(convertToKebabCase(bookTitle)).toBe("how-to-train-your-dragon");
+		});
+	});
+	describe("removeDuplicatesFromArray", () => {
+		it("different arrays", () => {
+			const arr1 = ["Hello", "world"];
+			const arr2 = ["Goodbye", "friends"];
+
+			expect(removeDuplicatesFromArray(arr1, arr2)).toStrictEqual([
+				"Hello",
+				"world",
+				"Goodbye",
+				"friends",
+			]);
+		});
+		it("one matching element", () => {
+			const arr1 = ["Hello", "world"];
+			const arr2 = ["Goodbye", "world"];
+
+			expect(removeDuplicatesFromArray(arr1, arr2)).toStrictEqual([
+				"Hello",
+				"world",
+				"Goodbye",
+			]);
+		});
+		it("multiple matching elements", () => {
+			const arr1 = ["Hello", "sweet", "world"];
+			const arr2 = ["Goodbye", "sweet", "world"];
+
+			expect(removeDuplicatesFromArray(arr1, arr2)).toStrictEqual([
+				"Hello",
+				"sweet",
+				"world",
+				"Goodbye",
+			]);
+		});
+		it("all matching elements", () => {
+			const arr1 = ["Hello", "sweet", "world"];
+			const arr2 = ["Hello", "sweet", "world"];
+
+			expect(removeDuplicatesFromArray(arr1, arr2)).toStrictEqual([
+				"Hello",
+				"sweet",
+				"world",
+			]);
 		});
 	});
 });
