d5f034367fe1baa5700893c4abacf5250cfdd448
Katie <kitkatkadams@gmail.com>
2025-11-07 14:42:52 +0000

    Undo unnecessary correction

--- src/components/ListItems/ShowListItems.tsx
+++ src/components/ListItems/ShowListItems.tsx
@@ -5,31 +5,21 @@ import { ListContext } from "../../reducers/context";
 
 export const ShowListItems = () => {
 	const list = useContext(ListContext);
-	// const listItemCards = list.map((item: ListItemInterface) => (
-	// 	<ListItem
-	// 		key={item.id}
-	// 		id={item.id}
-	// 		text={item.content}
-	// 		category={item.category}
-	// 		isChecked={item.isComplete}
-	// 	/>
-	// ));
+	const listItemCards = list.map((item: ListItemInterface) => (
+		<ListItem
+			key={item.id}
+			id={item.id}
+			text={item.content}
+			category={item.category}
+			isChecked={item.isComplete}
+		/>
+	));
 
 	return (
 		<section className="flex flex-col gap-y-2">
 			<h2>Your 2Do List</h2>
 			<hr />
-
-			{list?.map((item: ListItemInterface) => (
-				<ListItem
-					key={item.id}
-					id={item.id}
-					text={item.content}
-					category={item.category}
-					isChecked={item.isComplete}
-				/>
-			))}
-			{/* <div className="flex flex-col gap-y-1">{listItemCards}</div> */}
+			<div className="flex flex-col gap-y-1">{listItemCards}</div>
 		</section>
 	);
 };
