77891bbf7a0da34063f555443c4e427b2fa81690
Kit <kitella1@users.noreply.github.com>
2025-11-09 16:16:07 +0000

    Update context.ts
    Added explanatory comment for groupings

--- src/reducers/context.ts
+++ src/reducers/context.ts
@@ -3,8 +3,11 @@ import type { ListItemInterface } from "../types/listItems";
 import type { ActionsInterface as ListActionsInterface } from "./listReducer";
 import type { ActionsInterface as FilterActionsInterface } from "./filterReducer";
 
+/* Contexts */
 export const ListContext = createContext<ListItemInterface[]>(null);
 export const FiltersContext = createContext<string[]>(null);
+
+/* Dispatch Contexts */
 export const ListDispatchContext =
 	createContext<ActionDispatch<[action: ListActionsInterface]>>(null);
 export const FiltersDispatchContext =
